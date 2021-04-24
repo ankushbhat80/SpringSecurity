57d416caf8efdb8c51b56830c1014c6965678de1
Ankush <ankushbhat80@gmail.com>
2021-04-24 14:27:36 +0530

    Second commit

--- src/main/java/com/eazybytes/controller/AccountController.java
+++ src/main/java/com/eazybytes/controller/AccountController.java
@@ -3,6 +3,8 @@ package com.eazybytes.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+//RestController
 @RestController
 public class AccountController {
 	
